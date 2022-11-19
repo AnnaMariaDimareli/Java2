9af33cd288a64043a5eb56a94f26c267457327be
Panos Dask <kkKGHNYm9gJb8i8>
2022-11-19 11:38:42 +0200

    Multiple issues in Post.java Added package statement Simplified line 38 Replaced "==" with null-safe equals (Line 55, 61 and line 3 for import statement) Added required return null; at line 81

--- src/main/java/gr/aueb/Post.java
+++ src/main/java/gr/aueb/Post.java
@@ -1,66 +1,69 @@
+package gr.aueb;
+
+import java.util.Objects;
 import java.util.Scanner ;
 import java.util.Date;
 public class Post {
     Employer emp = new Employer();
-    
-    private String creator ; 
+
+    private String creator ;
     
     this.creator = emp.getName() +" "+ emp.getSurname() ;
-    
+
     private int likeCount = 0 ;
-    
+
     private boolean available = false ;
-    
+
     Date dt = new Date(System.currentTimeMillis());
-    
-    private String creationDate; //The creation date of the certain post 
-    
+
+    private String creationDate; //The creation date of the certain post
+
     public int getLikeCount() {
-    
+
         return likeCount;
-    
+
     }
-    
-    public void like(){ //if an employee wants to like a post then Employee class calls setLikeCount method to increase post's number of likes// 
-    
+
+    public void like(){ //if an employee wants to like a post then Employee class calls setLikeCount method to increase post's number of likes//
+
     this.likeCount++;
-    
+
     }
     public String getCreator(){
         return creator;
     }
-    
-    public void getAvailable() {
-    
-        if (this.available == true) {
-    
-            System.out.print("The post is available") ; 
 
-    
+    public void getAvailable() {
+
+        if (this.available) {
+
+            System.out.print("The post is available") ;
+
+
         } else {
-    
+
             System.out.print("The post is not available");
-    
+
         }
-    
+
     }
-    /* if the employer wants to make the post available then calls setAvailable with argument YES 
-     * if he wants to set it as unavailable then calls setAvailable with argument NO 
+    /* if the employer wants to make the post available then calls setAvailable with argument YES
+     * if he wants to set it as unavailable then calls setAvailable with argument NO
      */
-    public void setAvailable(String answer) { 
-    
-        if ( answer == "YES") {
-    
+    public void setAvailable(String answer) {
+
+        if (Objects.equals(answer, "YES")) {
+
             this.available = true ;
 
             emp.addUploadedPosts(this); //CHECK THE ARGUMENT.
-    
-        } else if ( answer == "NO") {
-    
+
+        } else if (Objects.equals(answer, "NO")) {
+
             this.available = false ;
-    
+
         }
-    
+
     }
     /*createPost method offers to Employers a template in order to create their job offering post .
      * If they just want to create a post to share their ideas about the labor market they choose the second option
@@ -68,21 +71,22 @@ public class Post {
     public String createPost() {
         System.out.println("Do you want to create a job offering post or share your thoughts about the labor market ?. Type 1 or 2");
         this.creationDate = dt.toString();
-    
+
         Scanner input = new Scanner(System.in);
         if (input.nextInt() == 1 ) {
             return String.format("Job Title : %s /n Company : %s /n Workplace type(On-site , Hybrid , Remote) : %s /n Job Location : %s /n Salary Range : %d - %d /n Description : %s /n" ,  input.nextLine() , input.nextLine() ,  , input.nextLine() , input.nextLine() , input.nextInt() , input.nextInt() ,  input.nextLine() );
         } else if ( input.nextInt() == 2 ) {
-            return String.format("Share your thoughts with your network . /n %s" , input.nextLine()) ; 
+            return String.format("Share your thoughts with your network . /n %s" , input.nextLine()) ;
         }
+        return null;
     }
 
     @Override
-    
+
     public String toString() {
-    
+
         return String.format("This post was created on %s by %s" , this.creationDate , this.creator);
-    
+
     }
 
 
