b2ed30f6b9aca950c016c6e1a8c18c8eb4efbea6
Panos Dask <kkKGHNYm9gJb8i8>
2022-11-19 11:41:42 +0200

    Fixed styling issues Fixed styling issues in accordance with recognised practices

--- src/main/java/gr/aueb/Post.java
+++ src/main/java/gr/aueb/Post.java
@@ -1,21 +1,17 @@
 package gr.aueb;
 
-import java.util.Objects;
-import java.util.Scanner ;
 import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Post {
     Employer emp = new Employer();
-
-    private String creator ;
-    
-    this.creator = emp.getName() +" "+ emp.getSurname() ;
-
-    private int likeCount = 0 ;
-
-    private boolean available = false ;
-
     Date dt = new Date(System.currentTimeMillis());
-
+    
+    this.creator =emp.getName()+" "+emp.getSurname();
+    private String creator;
+    private int likeCount = 0;
+    private boolean available = false;
     private String creationDate; //The creation date of the certain post
 
     public int getLikeCount() {
@@ -24,12 +20,13 @@ public class Post {
 
     }
 
-    public void like(){ //if an employee wants to like a post then Employee class calls setLikeCount method to increase post's number of likes//
+    public void like() { //if an employee wants to like a post then Employee class calls setLikeCount method to increase post's number of likes//
 
-    this.likeCount++;
+        this.likeCount++;
 
     }
-    public String getCreator(){
+
+    public String getCreator() {
         return creator;
     }
 
@@ -37,7 +34,7 @@ public class Post {
 
         if (this.available) {
 
-            System.out.print("The post is available") ;
+            System.out.print("The post is available");
 
 
         } else {
@@ -47,6 +44,7 @@ public class Post {
         }
 
     }
+
     /* if the employer wants to make the post available then calls setAvailable with argument YES
      * if he wants to set it as unavailable then calls setAvailable with argument NO
      */
@@ -54,29 +52,30 @@ public class Post {
 
         if (Objects.equals(answer, "YES")) {
 
-            this.available = true ;
+            this.available = true;
 
             emp.addUploadedPosts(this); //CHECK THE ARGUMENT.
 
         } else if (Objects.equals(answer, "NO")) {
 
-            this.available = false ;
+            this.available = false;
 
         }
 
     }
+
     /*createPost method offers to Employers a template in order to create their job offering post .
      * If they just want to create a post to share their ideas about the labor market they choose the second option
-    */
+     */
     public String createPost() {
         System.out.println("Do you want to create a job offering post or share your thoughts about the labor market ?. Type 1 or 2");
         this.creationDate = dt.toString();
 
         Scanner input = new Scanner(System.in);
-        if (input.nextInt() == 1 ) {
-            return String.format("Job Title : %s /n Company : %s /n Workplace type(On-site , Hybrid , Remote) : %s /n Job Location : %s /n Salary Range : %d - %d /n Description : %s /n" ,  input.nextLine() , input.nextLine() ,  , input.nextLine() , input.nextLine() , input.nextInt() , input.nextInt() ,  input.nextLine() );
-        } else if ( input.nextInt() == 2 ) {
-            return String.format("Share your thoughts with your network . /n %s" , input.nextLine()) ;
+        if (input.nextInt() == 1) {
+            return String.format("Job Title : %s /n Company : %s /n Workplace type(On-site , Hybrid , Remote) : %s /n Job Location : %s /n Salary Range : %d - %d /n Description : %s /n", input.nextLine(), input.nextLine(), , input.nextLine(), input.nextLine(), input.nextInt(), input.nextInt(), input.nextLine());
+        } else if (input.nextInt() == 2) {
+            return String.format("Share your thoughts with your network . /n %s", input.nextLine());
         }
         return null;
     }
@@ -85,11 +84,9 @@ public class Post {
 
     public String toString() {
 
-        return String.format("This post was created on %s by %s" , this.creationDate , this.creator);
+        return String.format("This post was created on %s by %s", this.creationDate, this.creator);
 
     }
-
-
 
 
 }
