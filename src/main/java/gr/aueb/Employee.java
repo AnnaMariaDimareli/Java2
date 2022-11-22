af3cb020605b943bcd5e067d1e689ca030f69ffd
NickRagousis <t8210127@aueb.gr>
2022-11-22 16:29:56 +0200

    Employee added

---
+++ src/main/java/gr/aueb/Employee.java
@@ -0,0 +1,75 @@
+package gr.aueb;
+
+import java.util.ArrayList;
+
+public class Employee extends User {
+	
+	private ArrayList <Post> likedPosts = new ArrayList<Post>();
+	
+	
+	//TODO
+	public Employee(String username, String password, String name, String surname, String email, String dateOfBirth) {
+		super(username, password, name, surname, email, dateOfBirth);
+	}
+	
+	
+	public void addLikedPosts(Post x) {
+		likedPosts.add(x);
+	}
+	
+	
+	public void sizeOfLikedPosts() {
+		System.out.println("You have liked " + likedPosts.size() + " posts!");
+	}
+	
+	
+	public void unlike(Post x) {
+		likedPosts.remove(x);
+	}
+	
+	
+	public ArrayList<Post> getLikedPosts() {
+		return likedPosts;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return (super.toString() + " and you are an employee-type user.");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
