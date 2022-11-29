c0ad3b6f2862919a043fd4cab608469ec341ec13
simosathan9 <simosathan03@gmail.com>
2022-11-29 21:18:52 +0200

    1.Improved formatting issues 2.Divided createPost in two different methods 3.Put "reftoemp" variable that refers to an object of type Employer in order get access to it's methods and fields

--- src/main/java/gr/aueb/Post.java
+++ src/main/java/gr/aueb/Post.java
@@ -7,24 +7,22 @@ import java.util.Scanner;
 public class Post {
     Date dt = new Date(System.currentTimeMillis());
     private String creator;
-    public Post(Employer emp){
-  
-        creator = emp.getName() + " " + emp.getSurname();
-    }
+    Employer reftoemp;
+    Scanner input = new Scanner(System.in);
     private int likeCount = 0;
     private boolean available = false;
-    private String creationDate; //The creation date of the certain post
+    private String creationDate;
+    public Post(Employer emp) {
+        reftoemp = emp;
+        creator = reftoemp.getName() + " " + emp.getSurname();
+    }
 
     public int getLikeCount() {
-
         return likeCount;
-
     }
 
     public void like() { //if an employee wants to like a post then Employee class calls setLikeCount method to increase post's number of likes//
-
         likeCount++;
-
     }
 
     public String getCreator() {
@@ -32,64 +30,37 @@ public class Post {
     }
 
     public void getAvailable() {
-
         if (available) {
-
             System.out.print("The post is available");
-
-
         } else {
-
             System.out.print("The post is not available");
-
         }
-
     }
-
     /* if the employer wants to make the post available then calls setAvailable with argument YES
      * if he wants to set it as unavailable then calls setAvailable with argument NO
      */
     public void setAvailable(String answer) {
-
         if (Objects.equals(answer, "YES")) {
-
             available = true;
-
-            emp.addUploadedPosts(this); //TODO CHECK THE ARGUMENT.
-
+            reftoemp.addUploadedPosts(this); //TODO CHECK THE ARGUMENT.
         } else if (Objects.equals(answer, "NO")) {
-
             available = false;
-
         }
-
     }
-
-    /*createPost method offers to Employers a template in order to create their job offering post .
-     * If they just want to create a post to share their ideas about the labor market they choose the second option
-     */
-    public String createPost() {
-        System.out.println("Do you want to create a job offering post or share your thoughts about the labor market ?. Type 1 or 2");
+    public String jobOfferingPost() {
         creationDate = dt.toString();
-
-        Scanner input = new Scanner(System.in);
-        if (input.nextInt() == 1) {
-            return String.format("Job Title : %s /n Company : %s /n Workplace type(On-site , Hybrid , Remote) : %s /n Job Location : %s /n Salary Range : %d - %d /n Description : %s /n", input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine(), input.nextInt(), input.nextInt(), input.nextLine());
-        } else if (input.nextInt() == 2) {
-            return String.format("Share your thoughts with your network . /n %s", input.nextLine());
-        }
-        return null;
+        return String.format("Job Title : %s /nCompany : %s /nWorkplace type(On-site , Hybrid , Remote) : %s /nJob Location : %s /nSalary Range : %d - %d /nDescription : %s /n", input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine(), input.nextInt(), input.nextInt(), input.nextLine());
+    }
+    public String thoughtsSharingPost() {
+        creationDate = dt.toString();
+        return String.format("Share your thoughts with your network . /n %s", input.nextLine());
     }
 
     @Override
 
     public String toString() {
-
-        return String.format("This post was created on %s by %s", creationDate, creator);
-
+        return String.format("This post was created on %s by %s/n", creationDate, creator);
     }
-
-
 }
 
 
