fe9ca573784d5bf3a6ba2b5f3704b5ebeca85ea1
Greg Terzidis <t8210148@aueb.gr>
2022-12-04 13:51:43 +0200

    Replaced reply method with 3 other methods for clarity

--- src/main/java/gr/aueb/Message.java
+++ src/main/java/gr/aueb/Message.java
@@ -14,13 +14,13 @@ public class Message {
     public Message(String receiver, String sender, String contents) {
         this.receiver = receiver;
         this.sender = sender;
-        seen = false; 
+        seen = false;
         numberOfMessages++;
         this.contents = contents;
     }
     /* The second constructor should be used for Automated messages
      */
-    public Message(String receiver, String sender, String creator, String email) { 
+    public Message(String receiver, String sender, String creator, String email) {
         this.receiver = receiver;
         this.sender = sender;
         seen = false;
@@ -28,30 +28,35 @@ public class Message {
         contents = this.sendAutomatedMessage(creator, email);
     }
 
+      public Message(String receiver, String sender) {
+	        this.receiver = receiver;
+	        this.sender = sender;
+	        seen = false;
+	        numberOfMessages++;
+    }
+
     public String toString() {
             return  "The receiver is " + receiver + ",  the sender is " + sender + " and the message is " + contents;
 
     }
-    /* reply method provides the ability to enter the message to the user.
-     */
 
-    public String reply(String sender, String receiver) { // 
-        this.sender = sender;
-        this.receiver = receiver;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your message:");
-        contents = input.nextLine();
-        seen = false;
-        messageNumber++;
-        return this.toString();
+    public void enterMessage(String sender, String receiver) {
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("Please enter your message:");
+	        contents = input.nextLine();
+	    }
+
+	    public void showMessage() {
+	        System.out.println(contents);
     }
+
     /* sendAutomatedMessage method creates an automated message,
      * after an employee likes a post.
      * sendAutomatedMessage requires as a parameter the name and email of the post's creator.
      */
-    public String sendAutomatedMessage(String creator, String email) { 
-        String x = "Thank you for your interest in our job offering. You can send your cv in Mr/Mrs " + creator + " email: " + email; 
-        System.out.println(x); 
+    public String sendAutomatedMessage(String creator, String email) {
+        String x = "Thank you for your interest in our job offering. You can send your cv in Mr/Mrs " + creator + " email: " + email;
+        System.out.println(x);
         return x;
     }
 
@@ -66,7 +71,7 @@ public class Message {
     public String getSender() {
         return sender;
     }
-    
+
     public String getReceiver() {
         return receiver;
     }
