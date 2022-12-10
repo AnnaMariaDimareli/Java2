272d791ace5926d782a6f9401b605aeedd54798a
Panos Dask <kkKGHNYm9gJb8i8>
2022-12-10 13:00:05 +0200

    First commit of ExecutingClass This class is to help with debugging and laying out App class

---
+++ src/main/java/gr/aueb/ExecutingClass.java
@@ -0,0 +1,10 @@
+package gr.aueb;
+
+public class ExecutingClass {
+    /**
+     * This class helps with the layout of App.java
+     * Its considered an integral part of it
+     * It should not be viewed as stand-alone class
+     *
+     */
+}
