5f7fdfee0e43c7a40c9cd1d98d756d8dd6ea3308
simosathan9 <simosathan03@gmail.com>
2022-12-21 12:47:32 +0200

    This class is supposed to test all Post.java methods.

---
+++ src/test/java/gr/aueb/PostTest.java
@@ -0,0 +1,34 @@
+package gr.aueb;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PostTest {
+    private Employer employer = new Employer("simosathan9", "jhnksxj", "Simos", "Athanasiadis", "simosathan03@gmail.com",
+        "9-11-2003");
+    private Post post = new Post(employer);
+
+    @Test
+    public void testGetReferenceToEmployer() {
+        assertEquals(employer, post.getReferenceToEmployer());
+    }
+
+    @Test
+    public void testLike() {
+        post.like();
+        assertEquals(1, post.getLikeCount());
+    }
+
+    @Test
+    public void testGetCreator() {
+        assertEquals("Simos Athanasiadis", post.getCreator());
+    }
+
+    @Test
+    public void testSetAvailable() {
+        post.setAvailable("YES");
+        assertEquals("The post is available", post.getAvailable());
+
+        post.setAvailable("NO");
+        assertEquals("The post is not available", post.getAvailable());
+    }
+}
