6cbf96911801489676f9a0420fb7f158afd10de8
simosathan9 <simosathan03@gmail.com>
2022-12-23 16:22:14 +0200

    Updated case 5 in the main method

--- src/main/java/gr/aueb/App.java
+++ src/main/java/gr/aueb/App.java
@@ -7,61 +7,63 @@ package gr.aueb;
 
 public class App {
     public static void main(String[] args) {
-        //No user created yet
+        //no user created yet
         User currentUser = null;
         ExecutingClass runner = new ExecutingClass();
-        //Show the welcome menu
+        //show the welcome menu
         runner.welcome();
         int selection;
 
-        //Keep the interface open untill the user makes a choice
+        //keep the interface open untill the user makes a choice
         do {
 
             do {
-                //Show user's choices 
+                //show user's choices 
                 selection = runner.printMainMenu();
             } while (selection != 1 && selection != 2 && selection != 3);
 
             switch (selection) {
-                //Initiate the signup process if selection is 1
+                //initiate the signup process if selection is 1
                 case 1:
                     currentUser = runner.signup();
                     break;
-                 //Initiate the login process if selection is 2    
+                 //initiate the login process if selection is 2    
                 case 2:
                     boolean flag;
                     currentUser = runner.login();
                     break;
-                //Terminate the app if selection is 3
+                //terminate the app if selection is 3
                 case 3:
                     runner.exit();
             }
 
             do {
                 do {
-                    //Print the Home Screen Menu
+                    //print the Home Screen Menu
                     selection = runner.printUserHomeScreen(currentUser);
                 } while (selection != 1 && selection != 2 && selection != 3 && selection != 4 && selection != 5);
                 switch (selection) {
-                    //Initiate the process if selection is 1
+                    //initiate the process if selection is 1
                     case 1:
-                        runner.sendNewMessage(currentUser);
+
                         break;
-                    //Initiate the process if selection is 2
+                    //initiate the process if selection is 2
                     case 2:
 
                         break;
-                    //Terminate the app if selection is 3
+                    //terminate the app if selection is 3
                     case 3:
                         runner.exit();
                         break;
-                    //Initiate the process if selection is 4
+                    //initiate the process if selection is 4
                     case 4:
 
                         break;
-                    //Initiate the process if selection is 5
+                    //initiate the process if selection is 5
                     case 5:
-
+                        if (currentUser instanceof Employer) {
+                            runner.postCreator((Employer) currentUser);
+                        }
                 }
             } while (selection != 3);
 
