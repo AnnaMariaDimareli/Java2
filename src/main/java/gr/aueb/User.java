86c45dddb5fdd7bc2f5c6b7084256b0449b32f44
MarkellaEgglezou <t8210039@aueb.gr>
2022-12-23 16:19:47 +0200

    Deleted unused method

--- src/main/java/gr/aueb/User.java
+++ src/main/java/gr/aueb/User.java
@@ -120,10 +120,7 @@ public class User {
 		return String.format("Your username is %s.", username);
 	}
 
-	protected ArrayList<Message> getGetUnseenMessages() {
-		return unseenMessages;
-	}
-
+	//Gives User object using hashmap
 	protected User getUserFromUsername(String username) {
 		return createdUsers.get(username);
 	}
