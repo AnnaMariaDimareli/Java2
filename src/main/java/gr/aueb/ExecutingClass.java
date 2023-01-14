ed9a4bc46bb0169ac1d39404f05dfe885ca2fee5
Panos Dask <panosdaskalopoulos259@gmail.com>
2023-01-14 09:35:43 +0200

    Regex updated (again) in accordance to RFC 5322

--- src/main/java/gr/aueb/ExecutingClass.java
+++ src/main/java/gr/aueb/ExecutingClass.java
@@ -162,8 +162,18 @@ public final class ExecutingClass {
         do {
             email = input.nextLine();
             //regex in accordance with RFC 5322
-            String emailPattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]"
-                    + "+@[a-zA-Z0-9.-]+$";
+            String emailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\"
+                    + ".[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
+                    + "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23"
+                    + "-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c"
+                    + "\\x0e-\\x7f])*\")@(?:(?:[a-z0-9]"
+                    + "(?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]"
+                    + "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:"
+                    + "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
+                    + "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9"
+                    + "-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f"
+                    + "\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b"
+                    + "\\x0c\\x0e-\\x7f])+)\\])";
             Pattern pattern = Pattern.compile(emailPattern);
             Matcher matcher = pattern.matcher(email);
             flag = matcher.matches();
